a416aee634696a1d934a1fa42fb68b285f8b1895
Rabia Alhaffar <steria773@gmail.com>
2021-04-17 00:39:36 +0300

    Update hello_ice_math.c

--- samples/ice_math/hello_ice_math.c
+++ samples/ice_math/hello_ice_math.c
@@ -3,8 +3,8 @@
 #include "ice_math.h"
 
 int main(int argc, char** argv) {
-	printf("Sine of 0.1 equals: %f\n\0", ice_math_sin(0.1));
-	printf("Cosine of 0.1 equals: %f\n\0", ice_math_cos(0.1));
-	printf("Tangent of 0.1 equals: %f\n\0", ice_math_tan(0.1));
-	return 0;
+    printf("Sine of 0.1 equals: %f\n\0", ice_math_sin(0.1));
+    printf("Cosine of 0.1 equals: %f\n\0", ice_math_cos(0.1));
+    printf("Tangent of 0.1 equals: %f\n\0", ice_math_tan(0.1));
+    return 0;
 }
