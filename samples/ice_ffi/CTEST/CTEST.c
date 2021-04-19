5b1f5b76d5a1c62c47827777b9e0fa523b93dd9f
Rabia Alhaffar <steria773@gmail.com>
2021-04-19 13:48:22 +0300

    Edited platform naming...

--- samples/ice_ffi/CTEST/CTEST.c
+++ samples/ice_ffi/CTEST/CTEST.c
@@ -5,10 +5,10 @@
 #include <stdio.h>
 
 #if defined(__WIN) || defined(_WIN32_) || defined(_WIN64_) || defined(WIN32) || defined(__WIN32__) || defined(WIN64) || defined(__WIN64__) || defined(WINDOWS) || defined(_WINDOWS) || defined(__WINDOWS) || defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__) || defined(_MSC_VER) || defined(__WINDOWS__) || defined(_X360) || defined(XBOX360) || defined(__X360) || defined(__X360__) || defined(_XBOXONE) || defined(XBONE) || defined(XBOX) || defined(__XBOX__) || defined(__XBOX) || defined(__xbox__) || defined(__xbox) || defined(_XBOX) || defined(xbox)
-#  define PLATFORM_WINDOWS
+#  define PLATFORM_MICROSOFT
 #endif
 
-#if defined(PLATFORM_WINDOWS)
+#if defined(PLATFORM_MICROSOFT)
 #  define DLLEXPORT __declspec(dllexport)
 #  define DLLIMPORT __declspec(dllimport)
 #else
